43029014e562874ba628d4f20d90d34c1476afca
pgoddijn <pgoddijn@gmail.com>
2018-01-29 04:37:43 +0100

    "Tweaked" the basic code

--- FC-28/analog.cpp
+++ FC-28/analog.cpp
@@ -3,7 +3,7 @@ int output_value ;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Reading From the Sensor ...");
+  Serial.println("Reading From the Sensor...");
   delay(2000);
   }
 
@@ -11,7 +11,7 @@ void loop() {
 
   output_value= analogRead(sensor_pin);
   output_value = map(output_value,550,0,0,100);
-  Serial.print("Mositure : ");
+  Serial.print("Moisture: ");
   Serial.print(output_value);
   Serial.println("%");
   delay(1000);
